8f4010ce51404c4c20a810734956114527f4b6ca
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2023-09-14 11:23:13 +0900

    [Silver IV] Title: 국영수, Time: 416 ms, Memory: 62036 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -7,10 +7,12 @@ N = int(input())
 arr_grade = []
 
 for i in range(N):
-    grade = input().split()
-    arr_grade.append(grade)
+    name,*score = input().split()
+    arr_grade.append((name,*map(int,score)))
 
-sorted_arr = sorted(arr_grade, key = lambda x :(-int(x[1]),int(x[2]),-int(x[3]),x[0]))
+
+sorted_arr = sorted(arr_grade, key = lambda x :(-x[1],x[2],-x[3],x[0]))
+
 
 for i in sorted_arr:
     print(i[0])
