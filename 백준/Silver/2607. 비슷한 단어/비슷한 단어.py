b5900afacf6d77e72ccc7cf12f84fe250d740acd
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2023-09-18 11:10:03 +0900

    [Silver III] Title: 비슷한 단어, Time: 80 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -0,0 +1,27 @@
+import copy
+from sys import stdin
+from collections import Counter
+input = stdin.readline
+
+N = int(input())
+
+answer = 0
+
+first_word = list(input().rstrip())
+
+for i in range(N-1):
+    count = 0
+    copy_first = copy.deepcopy(first_word)
+    word = input().rstrip()
+
+    for j in word:
+
+        if j in copy_first:
+            copy_first.remove(j)
+        else:
+            count+=1
+
+    if count<=1 and len(copy_first)<=1 :
+        answer+=1
+
+print(answer)
