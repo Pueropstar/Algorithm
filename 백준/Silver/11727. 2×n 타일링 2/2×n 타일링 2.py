1ce64eabdf81e03002a5b37498c55b11592269f2
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-03-08 15:32:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+input = stdin.readline
+
+N = int(input())
+
+dp = [0 for i in range(N+1)]
+
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2,N+1):
+    dp[i] = dp[i-1] + (2*dp[i-2])
+    
+print(dp[N]%10007)
