b7e90fd3d97ac1c3897c67e370e11835887c05b5
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-03-15 16:51:02 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -5,13 +5,16 @@ input = stdin.readline
 arr_num = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 ans = ''
+
 N,B = map(int,input().split())
 
 
 while N//B !=0:
-    ans+=arr_num[N%B]
+    ans = arr_num[N%B]+ ans
+    
     N = N//B
 
-ans+=arr_num[N]
+ans = arr_num[N] + ans
 
-print(ans[::-1])
+
+print(ans)
