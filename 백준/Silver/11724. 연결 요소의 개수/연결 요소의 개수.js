bb18eca958dfe244e195e582b447850a334695da
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-03-31 19:02:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 852 ms, Memory: 99136 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -8,30 +8,26 @@ function solution(arr) {
     const queue = [start];
     while (queue.length !== 0) {
       const v = queue.shift();
-
-      if (graph[v]) {
-        graph[v].forEach((data, index) => {
-          if (!visited[data]) {
-            visited[data] = true;
-            queue.push(data);
-          }
-        });
-      }
+      graph[v].forEach((el) => {
+        if (!visited[el]) {
+          visited[el] = true;
+          queue.push(el);
+        }
+      });
     }
   }
 
   const [N, M] = arr[0].split(" ").map(Number);
 
-  const graph = new Array(N+1).fill().map(() => []);
-
-  const visited = new Array(N+1).fill(false);
+  const graph = Array.from({ length: N + 1 }, () => []);
+  const visited = Array.from({ length: N + 1 }, () => false);
 
   let ans = 0;
 
   for (let i = 0; i < M; i++) {
     const [u, v] = input[i + 1].split(" ").map(Number);
-    if (graph[u]) graph[u].push(v);
-    if (graph[v]) graph[v].push(u);
+    graph[u].push(v);
+    graph[v].push(u);
   }
 
   for (let i = 1; i <= N; i++) {
