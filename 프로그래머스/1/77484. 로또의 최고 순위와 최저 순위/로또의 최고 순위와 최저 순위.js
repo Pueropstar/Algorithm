b583b40d3edf47e3708d74a047b41648dfffee74
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-04-02 15:32:17 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -2,8 +2,7 @@ function solution(lottos, win_nums) {
     var answer = [];
     let arr = [];
     let min=0, max = 0;
-    
-    const award ={6:1,5:2,4:3,3:4,2:5,1:6,0:6};
+    const award ={6:1,5:2,4:3,3:4,2:5,0:6};
     
     for(const num of lottos){
         if (win_nums.includes(num)){
@@ -11,9 +10,12 @@ function solution(lottos, win_nums) {
             max++;
         }
     }
-    arr = lottos.filter((el)=> el == 0);
+    arr = lottos.filter((el)=> el===0);
     max+=arr.length;
     answer.push(award[max])
     answer.push(award[min])
+    
+    
+    
     return answer;
 }
