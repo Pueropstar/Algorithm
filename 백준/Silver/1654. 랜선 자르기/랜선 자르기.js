215663eae9e8b290c501e9320402f943ea7ee438
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-09-25 13:19:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 11356 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -6,22 +6,26 @@ const input = fs
   .split("\n");
 
 const [K, N] = input.shift().split(" ").map(Number);
-const cables = input.map(Number).sort((a, b) => a - b);
+const sorted_arr = input.map(Number).sort((a, b) => a - b);
+
 
 let start = 0;
-let end = cables[cables.length - 1];
+
+let end = sorted_arr[sorted_arr.length - 1];
 
 while (start <= end) {
-  let mid = Math.floor((start + end) / 2); // 내림
+  let mid = Math.floor((start + end) / 2);
   
-  let lineNum = cables.reduce((acc, cur) => acc + Math.floor(cur / mid), 0);
-  // Q. Math.floor VS parsInt : Math.floor 연산이 더 빠르다고 한다. 이거때문에 처음에 틀렸나...? 뭐지 ㅠ
+  let sum_quo = sorted_arr.reduce(
+    (acc, data) => acc + Math.floor(data / mid),
+    0
+  );
 
-  if (lineNum >= N) {
+  if (sum_quo >= N) {
     start = mid + 1;
   } else {
     end = mid - 1;
   }
 }
 
-console.log(end); // 목표인 N 개의 랜선을 만들 수 있는 경우에서, end는 가장 큰 값이 보장됨 (start 부분을 늘리기 때문)
+console.log(end);
