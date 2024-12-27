692127285921d60e589083d8f4f23d62a4dc14d2
Pueropstar <109838831+Pueropstar@users.noreply.github.com>
2024-12-27 15:18:15 +0900

    [Silver V] Title: 돌 게임, Time: 152 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input[0]);
+
+if (N % 2 == 0) {
+  console.log("CY");
+} else {
+  console.log("SK");
+}
